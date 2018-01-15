f6df048b965607d51590076a4488cbc4d333087f
Chaviva Moshavi <chaviva.moshavi@gmail.com>
2018-01-15 19:44:21 +0200

    put back all options from 4 to 20

--- src/reversi_app/SettingsController.java
+++ src/reversi_app/SettingsController.java
@@ -37,7 +37,8 @@ public class SettingsController {
                                   "Blue", "Purple", "HotPink", "White", 
                                   "Brown", "Black", "Silver");
   private ObservableList<Integer> board_size_options = 
-      FXCollections.observableArrayList(4, 6, 8, 10, 12, 14, 16, 18, 20);
+      FXCollections.observableArrayList(4, 5, 6, 7, 8, 9, 10, 11, 12,
+                                    13, 14, 15, 16, 17, 18, 19, 20);
   /**
    * initialize settings screen with color options and board size options.
    */
